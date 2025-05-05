b221e072dea765ff32c444c1b4d9a2dcc1db42f8
bbshin817 <bbshintaro0817@gmail.com>
2025-05-05 13:26:00 +0900

    init

---
+++ push_swap.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbaba <sbaba@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 12:35:29 by sbaba             #+#    #+#             */
+/*   Updated: 2025/05/05 13:25:30 by sbaba            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_node
+{
+	int				value;
+	int				index;
+	struct s_node	*prev;
+	struct s_node	*next;
+} t_node;
+
+typedef struct s_stack
+{
+	t_node	*top;
+} t_stack;
+
+
+#endif
